801a7a90d5ead00cf8ab0969c31dd8e5992259a2
Schifferking <schifferking@hotmail.com>
2022-05-12 14:52:03 -0500

    Create get_array_nums function
    It receives a string and with the bytes method it returns an array
    of numbers (ASCII code).

---
+++ caesar_cipher.rb
@@ -0,0 +1,9 @@
+# Use reduce to iterate and return the string modified 
+# Check if num is between 65 and 90 or 97 and 122
+# "Ignore" if not in ranges
+# Use .chr to convert num to character
+
+def get_array_nums(str)
+  #Returns an array of numbers
+  str.bytes
+end
