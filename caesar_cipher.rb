fc28217793d305738f854d37c5c08570f4d3e466
Schifferking <schifferking@hotmail.com>
2022-05-13 15:02:31 -0500

    Create modify_number function
    This function handles the logic of the caesar cipher; it receives two
    parameters: a number that represents the character and another one
    that is the shift factor.
    
    When the number is not a letter, it just returns the character; in case
    its a letter and it passes the last letter with the shift factor, it
    calculate which letter should be and finally when the num does not
    pass the last letter, it just adds the shift factor. At the end it
    returns the character.
    
    In the caesar cipher with the each method it applies this function and
    appends the results in the modified_string variable and when its done
    it returns the result.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,30 @@
-# Use reduce to iterate and return the string modified 
-# Check if num is between 65 and 90 or 97 and 122
-# "Ignore" if not in ranges
-# Use .chr to convert num to character
-
 def get_array_nums(str)
   #Returns an array of numbers
   str.bytes
 end
 
+def modify_number(num, shift_factor)
+  # Do nothing when this is not a letter
+  if (num < 65 || (num >= 91 && num <= 96) || num > 122)
+    return num.chr
+  end
+
+  if (num + shift_factor >= 91 && num <= 90)
+    # Upcase letters
+    num = 65 + (shift_factor - (90 - num)) - 1
+  elsif (num + shift_factor >= 123)
+    # Lowercase letters
+    num = 97 + (shift_factor - (122 - num)) - 1
+  else 
+    num += shift_factor
+  end
+
+  num.chr
+end
+
 def caesar_cipher(str, shift_factor=1)
   modified_string = ""
   str_numbers = get_array_nums(str)
-  str_numbers.reduce { |modified_string, num| str + }
+  str_numbers.each { |num| modified_string += modify_number(num, shift_factor) }
+  modified_string
 end
