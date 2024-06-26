731e63db87c8c3880be6bb33789aa08d2986f497
Schifferking <101956382+Schifferking@users.noreply.github.com>
2024-06-26 15:11:33 -0600

    Refactor: modify_number function
    Created new calculate_ascii_number function
    that contains if else statements from
    modify_number function. It was done to make
    said function smaller. Also following rubocop
    suggestion. This function is now named
    get_character.
    
    Made some linting changes with rubocop (mostly
    style ones).

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,30 +1,35 @@
+# frozen_string_literal: true
+
 def get_array_nums(str)
-  #Returns an array of numbers
+  # Returns an array of numbers
   str.bytes
 end
 
-def modify_number(num, shift_factor)
-  # Do nothing when this is not a letter
-  if (num < 65 || (num >= 91 && num <= 96) || num > 122)
-    return num.chr
-  end
-
-  if (num + shift_factor >= 91 && num <= 90)
+def calculate_ascii_number(num, shift_factor)
+  if num + shift_factor >= 91 && num <= 90
     # Upcase letters
-    num = 65 + (shift_factor - (90 - num)) - 1
-  elsif (num + shift_factor >= 123)
+    65 + (shift_factor - (90 - num)) - 1
+  elsif num + shift_factor >= 123
     # Lowercase letters
-    num = 97 + (shift_factor - (122 - num)) - 1
-  else 
-    num += shift_factor
+    97 + (shift_factor - (122 - num)) - 1
+  else
+    # characters
+    num + shift_factor
   end
-
-  num.chr
 end
 
-def caesar_cipher(str, shift_factor=1)
-  modified_string = ""
+def get_character(num, shift_factor)
+  # Do nothing when this is not a letter
+  return num.chr if num < 65 || (num >= 91 && num <= 96) || num > 122
+
+  ascii = calculate_ascii_number(num, shift_factor)
+
+  ascii.chr
+end
+
+def caesar_cipher(str, shift_factor = 1)
+  modified_string = ''
   str_numbers = get_array_nums(str)
-  str_numbers.each { |num| modified_string += modify_number(num, shift_factor) }
+  str_numbers.each { |num| modified_string += get_character(num, shift_factor) }
   modified_string
 end
